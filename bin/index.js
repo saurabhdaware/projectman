0b9a167e7a1df9e46841972ec987839989d5657d
Saurabh Daware <saurabhdaware99@gmail.com>
2019-09-01 16:38:44 +0530

    [BETA] package configs changed

--- bin/index.js
+++ bin/index.js
@@ -8,15 +8,19 @@ program.version(require('../package.json').version);
 program
     .command('open [projectName]')
     .alias('o')
+    .description("Open one of your saved projects")
     .action(action.openProject);
 
 program
     .command('add [projectDirectory]')
+    .alias('save')
+    .description("Save current directory as a project")
     .action(action.addProject);
 
 program
     .command('edit')
+    .description("Edit settings.json")
     .action(action.editConfigurations);
 
-
+// Parse arguments
 program.parse(process.argv)
